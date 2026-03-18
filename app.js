1cd3643dd6c966e4dbf4e516d8b2c327a9924cc8
buyakawo <wissalelamoury@gmail.com>
2026-03-18 15:29:51 +0000

    add configs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('server running');
+import express from 'express';
+import { PORT } from './config/env.js';
+
+const app = express();
+
+// Testing
+app.get('/', (req, res) => {
+    res.send('Welcome to SAPI');
+});
+
+app.listen(PORT, () => {
+    console.log(`SAPI is running on http://localhost:${PORT}`)
+});
+
+export default app;
