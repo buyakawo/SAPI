fbf8eb2b17fddb4a1da7df917233489e6e36db53
buyakawo <wissalelamoury@gmail.com>
2026-03-18 21:55:15 +0000

    add db connection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -18,8 +19,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to SAPI');
 });
 
-app.listen(PORT, () => {
-    console.log(`SAPI is running on http://localhost:${PORT}`)
+app.listen(PORT, async() => {
+    console.log(`SAPI is running on http://localhost:${PORT}`);
+    await connectToDatabase()
 });
 
 export default app;
