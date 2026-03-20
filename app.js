1d269a76dc6516ec41d675a534cbeff2e85929cb
buyakawo <wissalelamoury@gmail.com>
2026-03-20 21:46:15 +0000

    authentication

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express();
+
+app.use(express.json());
 
 // Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+// MiddleWares
+app.use(errorMiddleware);
 
 // Testing
 app.get('/', (req, res) => {
